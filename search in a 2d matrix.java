3137dc8c418095da64bfb8f4dbf63058718fcb0a
Kartikeya Shukla <31617919+KartikeyaShukla911@users.noreply.github.com>
2024-05-17 08:57:55 +0530

    lc daily

--- search in a 2d matrix.java
+++ search in a 2d matrix.java
@@ -37,3 +37,25 @@ class Solution {
        return false;
     }
 }
+
+-------------------------------
+
+
+public boolean searchMatrix(int[][] matrix, int target) {
+        
+    int n = matrix.length,m=matrix[0].length,i,j;
+    i=0;j=n-1;
+
+    while(i<n && j>=0)
+    {
+        System.out.println("Ans: "+matrix[i][j]);
+        if(matrix[i][j]==target)
+            return true;
+        
+        else if(matrix[i][j] < target)
+            i++;
+        else
+            j--;
+    }
+    return false;
+}
